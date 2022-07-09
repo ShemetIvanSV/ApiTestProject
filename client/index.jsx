e43a818eebcc22fd38f80a11dbf7ebcffd6b3ed7
Алексей Квасов <alexeykvasov@MacBook-Air-Aleksej.local>
2022-07-09 13:34:21 +0300

    добавил новый учебный проект React

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,13 @@
+//import React from 'react';
+import PersonList from "./PersonList";
+
 const rootNode = document.getElementById("app1");    // элемент для рендеринга приложения React
     // получаем корневой элемент 
     const root = ReactDOM.createRoot(rootNode);
     // рендеринг в корневой элемент
     root.render(
         <h1>Hello React</h1>  // элемент, который мы хотим создать
+     );
+     root.render(
+         <PersonList/>
      );
